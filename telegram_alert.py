86213ec4341d0a278e612bb9f1a37fdcb446f3e2
venuparisipogu-3181 <venuparisipogu@gmail.com>
2026-01-29 20:34:15 +0530

    Update telegram_alert.py

--- telegram_alert.py
+++ telegram_alert.py
@@ -1,7 +1,7 @@
 # telegram_alert.py
 import requests
 
-def send_telegram_alert(bot_token, chat_id, message):
+def send_telegram_alert(8289933882:AAGgTyAhFHYzlKbZ_0rvH8GztqXeTB6P-yQ, 2115666034, message):
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message}
     try:
