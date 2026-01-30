be506f1fd5212ca43f2809f5e2bbe96800877421
venuparisipogu-3181 <venuparisipogu@gmail.com>
2026-01-30 09:37:54 +0530

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,42 +1,13 @@
-# data_fetcher.py
-from truedata_ws.websocket.TD import TD
+from truedata_ws.websocket.TDWebSocket import TDWebSocket
 
-# --- TrueData credentials ---
-USERNAME = "venuparisipogu@gmail.com"
-PASSWORD = "sunandha@3181"
-LIVE_PORT = 8082
+td = None
 
-# --- Initialize TrueData connection ---
-td = TD(USERNAME, PASSWORD, live_port=LIVE_PORT)
-
-# --- Subscribe symbols ---
-SYMBOLS = ["NIFTY-I", "BANKNIFTY-I", "SENSEX-I"]
-
-# --- Data storage dictionary ---
-live_data = {
-    "NIFTY-I": {},
-    "BANKNIFTY-I": {},
-    "SENSEX-I": {}
-}
-
-# --- Tick callback function ---
-def on_tick(tick):
-    symbol = tick['symbol']
-    live_data[symbol] = {
-        "ltp": tick['ltp'],
-        "oi": tick['oi'],
-        "best_bid": tick['best_bid_price'],
-        "best_ask": tick['best_ask_price'],
-        "time": tick['time']
-    }
-
-# --- Register callback ---
-td.trade_callback(on_tick)
-
-# --- Start streaming ---
-td.start_live_data(SYMBOLS)
-
-# --- Optional stop function ---
-def stop_stream():
-    td.stop_live_data(SYMBOLS)
-    td.disconnect()
+def connect_truedata():
+    global td
+    if td is None:
+        td = TDWebSocket(
+            login_id="YOUR_ID",
+            password="YOUR_PASSWORD"
+        )
+        td.start_live_data()
+    return td
